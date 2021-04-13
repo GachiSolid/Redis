b01b4a0cc330b7ee9e70ae3e277b9ca6e7b73037
GachiSolid <miftahovsalavat@gmail.com>
2021-04-13 17:45:53 +0300

    init

---
+++ RedisGet.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new ConfigurationOptions();
+            options.EndPoints.Add("localhost", 6379);
+
+            var redis = ConnectionMultiplexer.Connect(options);
+            var db = redis.GetDatabase();
+
+            string command = "";
+            while (command != "0")
+            {
+                Console.Write("1.Key: ");
+                string key1 = Console.ReadLine();
+
+                Console.Write("2.Key: ");
+                string key2 = Console.ReadLine();
+
+                Console.WriteLine($"Result: {(int)db.StringGet(key1) * (int)db.StringGet(key2)}");
+
+                Console.WriteLine("Для выхода нажмите 0");
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RedisSet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new ConfigurationOptions();
+            options.EndPoints.Add("localhost", 6379);
+
+            var redis = ConnectionMultiplexer.Connect(options);
+            var db = redis.GetDatabase();
+
+            string command = "";
+            while (command != "0")
+            {
+                Console.Write("1.Key: ");
+                string key1 = Console.ReadLine();
+
+                Console.Write("1.Value: ");
+                var value1 = Console.ReadLine();
+
+                Console.Write("2.Key: ");
+                string key2 = Console.ReadLine();
+
+                Console.Write("2.Value: ");
+                var value2 = Console.ReadLine();
+
+                db.StringSet(key1, value1);
+                db.StringSet(key2, value2);
+
+                Console.WriteLine("Для выхода нажмите 0");
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
